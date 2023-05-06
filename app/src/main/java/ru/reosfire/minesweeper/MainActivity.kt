50b7f55d532596442e2df79b74ca0e510a960512
reosfire <2.com9761721@gmail.com>
2023-05-06 12:11:22 +0300

    View binding

--- app/src/main/java/ru/reosfire/minesweeper/MainActivity.kt
+++ app/src/main/java/ru/reosfire/minesweeper/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.reosfire.minesweeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.reosfire.minesweeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
