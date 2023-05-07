7dd54f56fe4dc97e91d4eae5341d175bfa887fdf
reosfire <2.com9761721@gmail.com>
2023-05-07 13:56:09 +0300

    Parcelize

--- app/src/main/java/ru/reosfire/minesweeper/game/GameSettings.kt
+++ app/src/main/java/ru/reosfire/minesweeper/game/GameSettings.kt
@@ -1,7 +1,11 @@
 package ru.reosfire.minesweeper.game
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameSettings(
     val width: Int,
     val height: Int,
     val minesCount: Int
-)
+) : Parcelable
