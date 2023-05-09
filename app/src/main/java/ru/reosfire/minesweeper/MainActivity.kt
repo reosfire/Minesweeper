f88b3addfe5b5b47a7ceabc85c4d74796be0df61
reosfire <2.com9761721@gmail.com>
2023-05-09 18:36:10 +0300

    Recycler view tests removed

--- app/src/main/java/ru/reosfire/minesweeper/MainActivity.kt
+++ app/src/main/java/ru/reosfire/minesweeper/MainActivity.kt
@@ -3,8 +3,6 @@ package ru.reosfire.minesweeper
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.reosfire.minesweeper.databinding.ActivityMainBinding
-import ru.reosfire.minesweeper.fragments.MainFragment
-import ru.reosfire.minesweeper.game.GameState
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,12 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        supportFragmentManager.beginTransaction()
-            .add(binding.fragmentContainer.id, MainFragment())
-            .commit()
-
-        val gamesAdapter = SavedGamesAdapter(Array(100) { GameState(10, 10, "asdfasdfasdf", "asdf") }.toMutableList())
-        binding.gamesList.adapter = gamesAdapter
     }
 }
